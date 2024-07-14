f95ff6f466fcfb5d19710261c6cd8befe499b2c8
Степан Бокарев <bokarevstepan1@yandex.ru>
2024-07-14 21:51:52 +0300

    Добавить логирование контроллера Item

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import ru.practicum.shareit.item.dto.ItemDto;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/items")
 public class ItemController {
@@ -27,6 +29,7 @@ public class ItemController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ItemDto addItem(@RequestHeader("X-Sharer-User-Id") Long userId, @Valid @RequestBody ItemDto itemDto) {
+        log.info("Received request to add item for user id: {}", userId);
         return itemService.addItem(userId, itemDto);
     }
 
@@ -34,21 +37,25 @@ public class ItemController {
     public ItemDto updateItem(@RequestHeader("X-Sharer-User-Id") Long userId,
                               @PathVariable Long itemId,
                               @RequestBody ItemDto itemDto) {
+        log.info("Received request to update item id: {} for user id: {}", itemId, userId);
         return itemService.updateItem(userId, itemId, itemDto);
     }
 
     @GetMapping("/{itemId}")
     public ItemDto getItem(@PathVariable Long itemId) {
+        log.info("Received request to get item id: {}", itemId);
         return itemService.getItem(itemId);
     }
 
     @GetMapping
     public List<ItemDto> getItemsByOwner(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Received request to get items for owner id: {}", userId);
         return itemService.getItemsByOwner(userId);
     }
 
     @GetMapping("/search")
     public List<ItemDto> searchItems(@RequestParam String text) {
+        log.info("Received request to search items with text: {}", text);
         return itemService.searchItems(text);
     }
 }
